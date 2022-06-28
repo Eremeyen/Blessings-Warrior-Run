7195a8bc44781652f6f66da20468f644d29216db
Eremeyen <erenonder1905@gmail.com>
2022-06-28 11:16:18 +0300

    "added csv file and added all attribute names but swords"

--- ImageCombiner.py
+++ ImageCombiner.py
@@ -2,6 +2,7 @@ from PIL import Image
 import os
 from os import listdir
 from random import random
+import csv
 
 #attribute folders:
 backgrounds = r"C:\Users\meren\OneDrive\Masaüstü\Blessings Warrior Run\Backgrounds"
@@ -20,92 +21,112 @@ finalImages = r"C:\Users\meren\OneDrive\Masaüstü\Blessings Warrior Run\Blessin
 
 #defining an empty images array which we'll use to come up with each final image
 images = []
-for i in range(len(os.listdir(r"C:\Users\meren\OneDrive\Masaüstü\Blessings Warrior Run"))-2):
+for i in range(len(os.listdir(r"C:\Users\meren\OneDrive\Masaüstü\Blessings Warrior Run"))-3):
     images.append("")
 
 background_names = {
-    "WARRIOR GRADIENT BACKGROUND 1.png":,
-    "WARRIOR GRADIENT BACKGROUND 2.png":,
-    "WARRIOR GRADIENT BACKGROUND 3.png":,
-    "WARRIOR GRADIENT BACKGROUND 4.png":,
-    "WARRIOR GRADIENT BACKGROUND 5.png":,
-    "WARRIOR GRADIENT BACKGROUND 6.png":,
-    "WARRIOR GRADIENT BACKGROUND 7.png":,
-    "WARRIOR GRADIENT BACKGROUND 8.png":,
-    "WARRIOR GRADIENT BACKGROUND 9.png":,
-    "WARRIOR GRADIENT BACKGROUND 10.png":,
-    "WARRIOR RARE BACKGROUND 1.png":,
-    "WARRIOR RARE BACKGROUND 3.png":,
-    "WARRIOR RARE BACKGROUND 4.png":,
-    "WARRIOR RARE BACKGROUND 2.png":,
-    "WARRIOR RARE BACKGROUND 5.png":
+    "WARRIOR GRADIENT BACKGROUND 1.png":"Navy gradient",
+    "WARRIOR GRADIENT BACKGROUND 2.png":"Purple gradient",
+    "WARRIOR GRADIENT BACKGROUND 3.png":"Blue gradient",    "WARRIOR GRADIENT BACKGROUND 4.png":"Gray gradient",
+    "WARRIOR GRADIENT BACKGROUND 5.png":"Yellow gradient",
+    "WARRIOR GRADIENT BACKGROUND 6.png":"Red gradient",
+    "WARRIOR GRADIENT BACKGROUND 7.png":"Green gradient",
+    "WARRIOR GRADIENT BACKGROUND 8.png":"Pink gradient",
+    "WARRIOR GRADIENT BACKGROUND 9.png":"Purple blue gradient",
+    "WARRIOR GRADIENT BACKGROUND 10.png":"Dawn gradient",
+    "WARRIOR RARE BACKGROUND 1.png":"Sunset",
+    "WARRIOR RARE BACKGROUND 3.png":"Moonray",
+    "WARRIOR RARE BACKGROUND 4.png":"Purple mist",
+    "WARRIOR RARE BACKGROUND 2.png":"Purple thunder",
+    "WARRIOR RARE BACKGROUND 5.png":"Orange thunder"
 }
+
+
 bWing_names = {
-    "WARRIOR BACK WING 1.png":,
-    "WARRIOR BACK WING 2.png":,
-    "WARRIOR BACK WING 3.png":,
-    "WARRIOR BACK WING 4.png":,
-    "WARRIOR BACK WING 5.png":
+    "WARRIOR BACK WING 2.png":"Black glitter back wing",
+    "WARRIOR BACK WING 3.png":"Gold wing",
+    "WARRIOR BACK WING 4.png":"Bordeaux back wing",
+    "WARRIOR BACK WING 5.png":"Light gray sparkle back wing",
+    "WARRIOR BACK WING 1.png":"Dark green back wing"
 }
 tail_names = {
-    "WARRIOR TAIL 1.png":,
-    "WARRIOR TAIL 2.png":,
-    "WARRIOR TAIL 3.png":,
-    "WARRIOR TAIL 4.png":,
-    "WARRIOR TAIL 5.png":
+    "WARRIOR TAIL 1.png":"White tail",
+    "WARRIOR TAIL 2.png":"Red tail",
+    "WARRIOR TAIL 4.png":"Dark tail",
+    "WARRIOR TAIL 3.png":"Dark red curly tail",
 }
 body_names = {
-    "WARRIOR BODY 4.png":,
-    "WARRIOR BODY 6.png":,
-    "WARRIOR BODY 1.png":,
-    "WARRIOR BODY 3.png":,
-    "WARRIOR BODY 7.png":,
-    "WARRIOR BODY 8.png":
+    "WARRIOR BODY 4.png":"Overcast",
+    "WARRIOR BODY 6.png":"Wine",
+    "WARRIOR BODY 1.png":"Overcast Stripe",
+    "WARRIOR BODY 7.png":"Thunder",
+    "WARRIOR BODY 8.png":"Spark"
 }
 eye_names = {
-    "WARRIOR EYE F1.png":,
-    "WARRIOR EYE F2.png":,
-    "WARRIOR EYE F3.png":,
-    "WARRIOR EYE F4.png":,
-    "WARRIOR EYE M1.png":,
-    "WARRIOR EYE M2.png":,
-    "WARRIOR EYE M3.png":,
-    "WARRIOR EYE M4.png":
+    "WARRIOR EYE F1.png":"Female blue eye",
+    "WARRIOR EYE F2.png":"Female aquamarine eye",
+    "WARRIOR EYE F3.png":"Female purple eye",
+    "WARRIOR EYE F4.png":"Female brown eye",
+    "WARRIOR EYE F5.png":"Female hazel eye",
+    "WARRIOR EYE M1.png":"Male blue eye",
+    "WARRIOR EYE M2.png":"Male aquamarine eye",
+    "WARRIOR EYE M3.png":"Male purple eye",
+    "WARRIOR EYE M4.png":"Male brown eye",
+    "WARRIOR EYE M5.png":"Male hazel eye"
 }
 fWing_names = {
-    "WARRIOR FRONT WING 1.png":,
-    "WARRIOR FRONT WING 2.png":,
-    "WARRIOR FRONT WING 3.png":,
-    "WARRIOR FRONT WING 4.png":,
-    "WARRIOR FRONT WING 5.png":
+    "WARRIOR FRONT WING 1.png":"Dark green front wing",
+    "WARRIOR FRONT WING 2.png":"Black glitter front wing",
+    "WARRIOR FRONT WING 3.png":"Gold wing",
+    "WARRIOR FRONT WING 4.png":"Bordeaux front wing",
+    "WARRIOR FRONT WING 5.png":"Light gray sparkle front wing"
 }
 hair_names = {
-    "WARRIOR HAIR 1.png":,
-    "WARRIOR HAIR 2.png":,
-    "WARRIOR HAIR 4.png":,
-    "WARRIOR HAIR 3.png":
+    "WARRIOR HAIR 1.png":"White hair",
+    "WARRIOR HAIR 2.png":"Red hair",
+    "WARRIOR HAIR 4.png":"Dark hair",
+    "WARRIOR HAIR 3.png":"Dark red curly hair"
 }
 horn_names = {
-    "WARRIOR HORN 3.png":,
-    "WARRIOR HORN 2.png":,
-    "WARRIOR HORN 4.png":,
-    "WARRIOR HORN 1.png":
+    "WARRIOR HORN 3.png":"Black spiral horn",
+    "WARRIOR HORN 2.png":"Black spiral arrow horn",
+    "WARRIOR HORN 4.png":"Red spiral arrow horn",
+    "WARRIOR HORN 1.png""Three tier horn:
 }
 helmet_names = {
-    "WARRIOR HELMET 1.png":,
-    "WARRIOR HELMET 2.png":,
-    "WARRIOR HELMET 3.png":,
-    "WARRIOR HELMET 4.png":,
-    "WARRIOR HELMET 5.png":,
-    "WARRIOR HELMET 6.png":,
-    "WARRIOR HELMET 7.png":,
-    "WARRIOR HELMET 8.png":,
-    "WARRIOR HELMET 9.png":
+    "WARRIOR HELMET 1.png":"Purple shimmer helmet",
+    "WARRIOR HELMET 2.png":"Purple helmet",
+    "WARRIOR HELMET 3.png":"Serene helmet",
+    "WARRIOR HELMET 4.png":"Ghost helmet",
+    "WARRIOR HELMET 5.png":"Golden helmet",
+    "WARRIOR HELMET 6.png":"Sriracha helmet",
+    "WARRIOR HELMET 7.png":"Lilac helmet",
+    "WARRIOR HELMET 8.png":"Bordeaux helmet",
+    "WARRIOR HELMET 9.png":"Foam helmet"
 }
+
+#a few swords have same file names
+#couldn't complete because I don't know how many swords there are
 sword_names = {
-    
+    "WARRIOR SWORD 3":"",
+    "WARRIOR SWORD Y":"",
+    "WARRIOR SWORD Z":"",
+    "WARRIOR SWORD 1:"",
+    "WARRIOR SWORD 5":"",
+    "WARRIOR SWORD ":"",
+    "WARRIOR SWORD ":"",
+    "WARRIOR SWORD ":"",
+    "WARRIOR SWORD ":"",
+    "WARRIOR SWORD ":"",
+    "WARRIOR SWORD ":""
 }
-accessory_names = {}
+accessory_names = {
+    "220329-0959-BOS LAYER.png":"None",
+    "WARRIOR ACC 1.png":"Black shawl",
+    "WARRIOR ACC 2.png":"Gorget",
+    "WARRIOR ACC 3.png":"Red Gorget",
+    "WARRIOR ACC 4.png":"Green Gorget"
+}
 
 
 
@@ -150,16 +171,14 @@ def whichBackground():
     
 def whichBackWing():
     n = random()
-    if(n < 0.2):
+    if(n < 0.28):
         return "WARRIOR BACK WING 1.png"
-    elif(n < 0.4):
+    elif(n < 0.56):
         return "WARRIOR BACK WING 2.png"
-    elif(n < 0.6):
-        return "WARRIOR BACK WING 3.png"
-    elif(n < 0.8):
+    elif(n < 0.85):
         return "WARRIOR BACK WING 4.png"
     else:
-        return "WARRIOR BACK WING 5.png"
+        return "WARRIOR BACK WING 3.png"
 def whichTail():
     n = random()
     if(n < 0.28):
@@ -173,15 +192,13 @@ def whichTail():
 
 def whichBody():
     n = random()
-    if(n < 0.3):
+    if(n < 0.33):
         return "WARRIOR BODY 4.png"
-    elif(n < 0.6):
+    elif(n < 0.66):
         return "WARRIOR BODY 6.png"
-    elif(n < 0.75):
+    elif(n < 0.84):
         return "WARRIOR BODY 1.png"
-    elif(n < 0.90):
-        return "WARRIOR BODY 3.png"
-    elif(n < 0.95):
+    elif(n < 0.92):
         return "WARRIOR BODY 7.png"
     else:
         return "WARRIOR BODY 8.png"
@@ -280,10 +297,21 @@ def whichAccessory():
 
 
 
-Metadata = {}
+Metadata = []
 
 while(bCounter < 2532):
     blessing = {}
+
+    #setting ID
+    if(bCounter < 10):
+        id = "000" + str(bCounter)
+    elif(bCounter < 100):
+        id = "00" + str(bCounter)
+    elif(bCounter < 1000):
+        id = "0" + str(bCounter)
+    else:
+        id = str(bCounter)
+    blessing["id"] = int(id)
 
     wBackground = whichBackground()
     images[0] = Image.open(backgrounds + "/" + wBackground).convert("RGBA")
@@ -340,14 +368,16 @@ while(bCounter < 2532):
     #print(str(bCounter) + " images generated")
     final_image.save(finalImages+"/Blessings Warrior " +str(bCounter)+".png")
     
-    if(bCounter < 10):
-        id = "Healer " + "000" + str(bCounter)
-    elif(bCounter < 100):
-        id = "Healer " + "00" + str(bCounter)
-    elif(bCounter < 1000):
-        id = "Healer "+ "0" + str(bCounter)
-    else:
-        id = str(bCounter)
-    Metadata[id] = blessing
+    
+    Metadata.append(blessing)
     
     bCounter += 1
+
+
+#writing csv file
+main_info = ["id","Background","Back Wing","Tail","Body","Eyes","Front Wing","Hair","Horn","Helmet","Sword","Accessory"]
+
+with open("metadata.csv", "w") as csvfile:
+    writer = csv.DictWriter(csvfile,fieldnames= main_info)
+    writer.writeHeader()
+    writer.writerows(Metadata)
